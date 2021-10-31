bbf8e980b6160e7c056dea1469742f8e022aeeb3
riadh <riad.amami@gmail.com>
2021-10-31 21:54:48 +0100

    add new stuff

--- src/main/java/tn/spring/timesheet/services/ContratServiceImpl.java
+++ src/main/java/tn/spring/timesheet/services/ContratServiceImpl.java
@@ -3,6 +3,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import tn.spring.timesheet.entities.Contrat;
+import tn.spring.timesheet.repository.ContratRepository;
+
 @Transactional
 @Service
 public class ContratServiceImpl implements IContratService {
